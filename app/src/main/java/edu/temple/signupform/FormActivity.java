ea2a0ec709f5152bc9e935d676db9392e87e2524
GrantGwiaz <61506873+GrantGwiaz@users.noreply.github.com>
2021-02-15 22:10:37 -0500

    Save onclick button shows appropriate errors

--- app/src/main/java/edu/temple/signupform/FormActivity.java
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -18,16 +18,46 @@ public class FormActivity extends AppCompatActivity {
         findViewById(R.id.buttonSave).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // reset errors to invisible upon pressing save
+                findViewById(R.id.TextViewPasswordConfirmErr).setAlpha(0);
+                findViewById(R.id.TextViewPasswordErr).setAlpha(0);
+                findViewById(R.id.TextViewEmailErr).setAlpha(0);
+                findViewById(R.id.TextViewNameErr).setAlpha(0);
+
+                //get all the views that had data entered by user
+                EditText nameText = (EditText) findViewById(R.id.editTextTextPersonName);
+                EditText emailText = (EditText) findViewById(R.id.editTextTextEmailAddress);
+                EditText passwordText = (EditText) findViewById(R.id.editTextTextPassword);
+                EditText confirmPasswordText = (EditText) findViewById(R.id.editTextTextPassword2);
+
+                // the actual strings of data entered
+                String name = nameText.getText().toString();
+                String email = emailText.getText().toString();
+                String password = passwordText.getText().toString();
+                String confirmPassword = confirmPasswordText.getText().toString();
+
                 boolean welcome = true;
 
-                String name = ( (EditText) findViewById(R.id.editTextTextPersonName)).getText().toString();
-                String email = ( (EditText) findViewById(R.id.editTextTextEmailAddress)).getText().toString();
-                String password = ( (EditText) findViewById(R.id.editTextTextPassword)).getText().toString();
-                String confirmPassword = ( (EditText) findViewById(R.id.editTextTextPassword2)).getText().toString();
+                if(name.contentEquals("")) {
+                    findViewById(R.id.TextViewNameErr).setAlpha(100);
+                    welcome = false;
+                }
+                if(email.contentEquals("")) {
+                    findViewById(R.id.TextViewEmailErr).setAlpha(100);
+                    welcome = false;
+                }
+                if(password.contentEquals("")) {
+                    findViewById(R.id.TextViewPasswordErr).setAlpha(100);
+                    welcome = false;
+                }
+                if(!password.contentEquals(confirmPassword)) {
+                    findViewById(R.id.TextViewPasswordConfirmErr).setAlpha(100);
+                    welcome = false;
+                }
+                if(welcome) {
+                    Toast.makeText(FormActivity.this, "Welcome, " + name + ", to the SignUpForm App", Toast.LENGTH_SHORT).show();
+                }
 
-
-                if(welcome)
-                Toast.makeText(FormActivity.this, "Welcome, " + "" + ", to the SignUpForm App", Toast.LENGTH_SHORT).show();
             }
         });
     }
