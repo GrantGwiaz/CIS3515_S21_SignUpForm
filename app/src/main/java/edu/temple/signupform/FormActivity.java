62c48b3164bb7abd500964aafb32b99b04595791
GrantGwiaz <61506873+GrantGwiaz@users.noreply.github.com>
2021-02-15 21:38:34 -0500

    Add Welcome Toast

--- app/src/main/java/edu/temple/signupform/FormActivity.java
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -3,6 +3,10 @@ package edu.temple.signupform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class FormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.buttonSave).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean welcome = true;
+
+                String name = ( (EditText) findViewById(R.id.editTextTextPersonName)).getText().toString();
+                String email = ( (EditText) findViewById(R.id.editTextTextEmailAddress)).getText().toString();
+                String password = ( (EditText) findViewById(R.id.editTextTextPassword)).getText().toString();
+                String confirmPassword = ( (EditText) findViewById(R.id.editTextTextPassword2)).getText().toString();
+
+
+                if(welcome)
+                Toast.makeText(FormActivity.this, "Welcome, " + "" + ", to the SignUpForm App", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
